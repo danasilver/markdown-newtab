5457051c2c6e960c3eed170884dbd73ff47af2ff
Vera Olsson <vera@tictail.com>
2015-12-02 10:50:38 +0100

    Convert tabs to spaces and adjust styling

--- markdown_newtab/__init__.py
+++ markdown_newtab/__init__.py
@@ -11,34 +11,47 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 from markdown import Extension, Markdown
 from markdown.inlinepatterns import \
-	LinkPattern, ReferencePattern, AutolinkPattern, AutomailPattern, \
-	LINK_RE, REFERENCE_RE, SHORT_REF_RE, AUTOLINK_RE, AUTOMAIL_RE
+    LinkPattern, ReferencePattern, AutolinkPattern, AutomailPattern, \
+    LINK_RE, REFERENCE_RE, SHORT_REF_RE, AUTOLINK_RE, AUTOMAIL_RE
 
 class NewTabMixin(object):
-	def handleMatch(self, m):
-		el = super(NewTabMixin, self).handleMatch(m)
-		if el is not None and not el.get('href').startswith('#'):
-			el.set('target', '_blank')
-		return el
+    def handleMatch(self, m):
+        el = super(NewTabMixin, self).handleMatch(m)
+        if el is not None and not el.get('href').startswith('#'):
+            el.set('target', '_blank')
+        return el
 
-class NewTabLinkPattern(     NewTabMixin, LinkPattern):      pass
-class NewTabReferencePattern(NewTabMixin, ReferencePattern): pass
-class NewTabAutolinkPattern( NewTabMixin, AutolinkPattern):  pass
-class NewTabAutomailPattern( NewTabMixin, AutomailPattern):  pass
+
+class NewTabLinkPattern(NewTabMixin, LinkPattern):
+    pass
+
+
+class NewTabReferencePattern(NewTabMixin, ReferencePattern):
+    pass
+
+
+class NewTabAutolinkPattern(NewTabMixin, AutolinkPattern):
+    pass
+
+
+class NewTabAutomailPattern(NewTabMixin, AutomailPattern):
+    pass
+
 
 class NewTabExtension(Extension):
-	"""Modifies HTML output to open links in a new tab."""
-	def extendMarkdown(self, md, md_globals):
-		md.inlinePatterns['link'] = \
-			NewTabLinkPattern(LINK_RE, md)
-		md.inlinePatterns['reference'] = \
-			NewTabReferencePattern(REFERENCE_RE, md)
-		md.inlinePatterns['short_reference'] = \
-			NewTabReferencePattern(SHORT_REF_RE, md)
-		md.inlinePatterns['autolink'] = \
-			NewTabAutolinkPattern(AUTOLINK_RE, md)
-		md.inlinePatterns['automail'] = \
-			NewTabAutomailPattern(AUTOMAIL_RE, md)
+    """Modifies HTML output to open links in a new tab."""
+    def extendMarkdown(self, md, md_globals):
+        md.inlinePatterns['link'] = \
+            NewTabLinkPattern(LINK_RE, md)
+        md.inlinePatterns['reference'] = \
+            NewTabReferencePattern(REFERENCE_RE, md)
+        md.inlinePatterns['short_reference'] = \
+            NewTabReferencePattern(SHORT_REF_RE, md)
+        md.inlinePatterns['autolink'] = \
+            NewTabAutolinkPattern(AUTOLINK_RE, md)
+        md.inlinePatterns['automail'] = \
+            NewTabAutomailPattern(AUTOMAIL_RE, md)
 
-def makeExtension(configs = {}):
-	return NewTabExtension(configs = configs)
+
+def makeExtension(configs={}):
+    return NewTabExtension(configs=configs)
